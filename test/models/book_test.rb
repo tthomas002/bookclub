0c76f6c44adda0313b6e6c488db58ff3b03454c5
tthomas002 <tim.thomas.cs@gmail.com>
2020-06-04 21:16:12 -0500

    add book title validation

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "must have title" do
+    assert_not Book.new.save
+  end
 end
