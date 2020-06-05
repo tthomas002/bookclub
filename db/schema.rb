266bbe6a1b57f29ccaa2b97ce740a28d7326df59
gratefulcheddar <timthomascode@icloud.com>
2020-06-04 22:09:17 -0500

    replace empty migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_023127) do
+ActiveRecord::Schema.define(version: 2020_06_05_030832) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_023127) do
     t.integer "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["book_id"], name: "index_chapters_on_book_id"
   end
 
