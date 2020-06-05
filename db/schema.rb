fd6bb2a4473f42a30ffb6cee67a91557fdbc22e4
tthomas002 <tim.thomas.cs@gmail.com>
2020-06-04 21:31:58 -0500

    add title to chapters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_022207) do
+ActiveRecord::Schema.define(version: 2020_06_05_023127) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
