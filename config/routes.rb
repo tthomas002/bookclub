91999130213340712c30dc8b2a0904c6922da85f
tthomas002 <tim.thomas.cs@gmail.com>
2020-06-11 16:39:36 -0500

    Change routes to use nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'books#index'
-  resources :books
-  resources :chapters
-  resources :posts
+  resources :books do
+    resources :chapters
+  end
+  resources :chapters do
+    resources :posts
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
