09ed3fa2b5bb0c14ee034e79229d48acb966443c
tthomas002 <tim.thomas.cs@gmail.com>
2020-06-11 14:57:46 -0500

    add content validation for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :chapter
   has_rich_text :content
+
+  validates :content, presence: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "content can't be blank" do
+    post_without_content = Post.new(chapter: chapters(:book_one_chapter_one))
+
+    assert_not post_without_content.save
+  end
 end
