e4f182891ee76dad0ed27d1283429ae1686254ac
tthomas002 <tim.thomas.cs@gmail.com>
2020-06-13 00:10:20 -0500

    add user/admin auth to books controller/views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: [:edit, :update, :destroy]
+
+  before_action :admin_authorization, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /books
   # GET /books.json
@@ -21,28 +23,20 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
 
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to books_path, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      redirect_to books_path, notice: 'Book was successfully created.' 
+    else
+      render :new 
     end
   end
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to books_path, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.update(book_params)
+      redirect_to books_path, notice: 'Book was successfully updated.'
+    else
+      render :edit 
     end
   end
 
@@ -50,10 +44,7 @@ class BooksController < ApplicationController
   # DELETE /books/1.json
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to books_url, notice: 'Book was successfully destroyed.' 
   end
 
   private
@@ -65,5 +56,9 @@ class BooksController < ApplicationController
     # Only allow a list of trusted parameters through.
     def book_params
       params.require(:book).permit(:title)
+    end
+
+    def admin_authorization
+      redirect_to books_path unless current_user.admin?
     end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,21 +1,48 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @book = books(:one)
+    @user = users(:user)
+    @admin = users(:admin)
   end
 
-  test "should get index" do
+  test "if a regular user is signed in, index loads" do
+    sign_in @user
     get books_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "if no user is signed in, index redirects to sign in" do
+    get books_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test "if regular user is signed in, new redirects to index" do
+    sign_in @user
+    get new_book_url
+    assert_redirected_to books_path
+  end
+
+  test "if admin is signed in, new loads" do
+    sign_in @admin
     get new_book_url
     assert_response :success
   end
 
-  test "should create book" do
+  test "if regular user is signed in, create fails, redirects to index" do
+    sign_in @user
+    assert_no_difference('Book.count') do
+      post books_url, params: { book: { title: @book.title } }
+    end
+    assert_redirected_to books_path
+  end
+
+  test "if admin is signed in, create succeeds" do
+    sign_in @admin
     assert_difference('Book.count') do
       post books_url, params: { book: { title: @book.title } }
     end
@@ -23,17 +50,49 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to books_url
   end
 
-  test "should get edit" do
+  test "if regular user is signed in, edit redirects to index" do
+    sign_in @user
+    get edit_book_url(@book)
+    assert_redirected_to books_path
+  end
+
+  test "if admin is signed in, edit succeeds" do
+    sign_in @admin
     get edit_book_url(@book)
     assert_response :success
   end
 
-  test "should update book" do
-    patch book_url(@book), params: { book: { title: @book.title } }
+  test "if regular user is signed in, update fails and redirects to index" do
+    sign_in @user
+
+    assert_no_changes('@book.updated_at') do
+      patch book_url(@book), params: { book: { title: "test" } }
+      @book.reload
+    end
+  end
+
+  test "if admin is signed in, update succeeds" do
+    sign_in @admin
+
+    assert_changes('@book.updated_at') do
+      patch book_url(@book), params: { book: { title: "test" } }
+      @book.reload
+    end
+  end
+
+  test "if regular user is signed in, destroy fails" do
+    sign_in @user
+    
+    assert_no_difference('Book.count') do
+      delete book_url(@book)
+    end
+
     assert_redirected_to books_url
   end
 
-  test "should destroy book" do
+  test "if admin is signed in, destroy succeeds" do
+    sign_in @admin
+
     assert_difference('Book.count', -1) do
       delete book_url(@book)
     end
