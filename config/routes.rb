c298d2d8f7ce56f0b97af0e5f9923af7c368de30
tthomas002 <tim.thomas.cs@gmail.com>
2020-06-13 01:19:17 -0500

    update routes for unused methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'books#index'
 
   resources :books, except: [:show] do
-    resources :chapters, except: [:new, :show]
+    resources :chapters, only: [:index, :create]
   end
-  resources :chapters, except: [:show, :create] do
-    resources :posts
+  resources :chapters, only: [:edit, :update, :destroy] do
+    resources :posts, except: [:new, :show]
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
